eb01c968967c922102bea147984994f3d6a14f91
san8holo <san8holo@gmail.com>
2016-02-07 11:26:40 +0100

    Update s8515.mk

--- s8515.mk
+++ s8515.mk
@@ -116,8 +116,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libnetcmdiface
 
-# Enable Widevine drmcd .
-PRODUCT_PROPERTY_OVERRIDES += drm.service.enabled=true
+
 
 # Light
 PRODUCT_PACKAGES += \
@@ -132,10 +131,12 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     make_ext4fs \
     setup_fs
+    
+# Enable Widevine drmcd .
+PRODUCT_PROPERTY_OVERRIDES += drm.service.enabled=true
 
 # Disable SELinux
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.boot.selinux=disabled
+PRODUCT_PROPERTY_OVERRIDES += ro.boot.selinux=disabled
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 PRODUCT_NAME := full_s8515
